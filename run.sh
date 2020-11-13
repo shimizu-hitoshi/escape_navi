2bc8ca56c9a9e42c366856009e54adf2aaebdda2
Hitoshi Shimizu <shimizu@zero-one.localdomain>
2020-11-13 15:43:21 +0900

    run.sh修正

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@
 # python main.py --save --configfn config_total_time_once.ini &
 
 # 手元で学習するために
-# python main.py --save --configfn config.ini
-python main.py --save --configfn config_zero_one.ini
+python main.py --save --configfn config.ini
+# python main.py --save --configfn config_zero_one.ini
 # python main.py --save --configfn config_time_once.ini
 # python main.py --save --configfn config_time.ini
 # python main.py --save --configfn config_zero_one.ini --checkpoint --inputfn logs/Curriculum/model
